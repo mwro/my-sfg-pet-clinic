fd5e7fb2c9dd7278c15fa2cc063120792954c229
Marcin <mwro91@gmail.com>
2019-04-13 23:05:15 +0200

    Implemented Map based Services. Closes #6.

---
+++ pet-clinic-data/src/main/java/guru/springframework/mysfgpetclinic/services/map/OwnerMapService.java
@@ -0,0 +1,43 @@
+package guru.springframework.mysfgpetclinic.services.map;
+
+import guru.springframework.mysfgpetclinic.model.Owner;
+import guru.springframework.mysfgpetclinic.services.OwnerService;
+
+import java.util.Map;
+import java.util.Set;
+
+public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return map.entrySet().stream()
+                .filter(e -> lastName.equals(e.getValue().getLastName()))
+                .map(Map.Entry::getValue)
+                .findFirst().orElse(null);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findByID(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/mysfgpetclinic/services/map/PetMapService.java
@@ -0,0 +1,33 @@
+package guru.springframework.mysfgpetclinic.services.map;
+
+import guru.springframework.mysfgpetclinic.model.Pet;
+import guru.springframework.mysfgpetclinic.services.PetService;
+
+import java.util.Set;
+
+public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findByID(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+}
